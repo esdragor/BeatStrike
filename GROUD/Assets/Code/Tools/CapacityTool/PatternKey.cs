47b6d6d43b709b0dcd1ea88823f7a88b63033123
Djason NATHIEZ <60692459+DjasonNathiez@users.noreply.github.com>
2023-02-27 17:16:14 +0100

    Timeline setup and begin tool editor

---
+++ GROUD/Assets/Code/Tools/CapacityTool/PatternKey.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Utilities;
+
+public class PatternKey : KeyClass
+{
+    public float drawSpeed;
+    public Vector3 spawnPosition;
+    public Enums.PatternType patternType;
+}
