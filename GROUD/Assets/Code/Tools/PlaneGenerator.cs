9a993b8823f945af526716d409f030c1b32ab4cb
baudouinlapeze <baudouinlapeze9@gmail.com>
2023-04-21 16:19:32 +0200

    build fix

--- GROUD/Assets/Code/Tools/PlaneGenerator.cs
+++ GROUD/Assets/Code/Tools/PlaneGenerator.cs
@@ -1,80 +1,83 @@
-using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 public class PlaneGenerator : MonoBehaviour
 {
-   public string meshName;
-   public Vector2 planeSize;
-   public int planeResolution;
+    public string meshName;
+    public Vector2 planeSize;
+    public int planeResolution;
 
-   private List<Vector3> vertices;
-   private List<int> triangles;
+    private List<Vector3> vertices;
+    private List<int> triangles;
 
-   private Mesh currentMesh;
+    private Mesh currentMesh;
 
-   [Button("Generate A Plane")]
-   public void GenerateMesh()
-   {
-      GeneratePlane(planeSize, planeResolution);
-      SaveMesh();
-   }
+    [Button("Generate A Plane")]
+    public void GenerateMesh()
+    {
+        GeneratePlane(planeSize, planeResolution);
+        SaveMesh();
+    }
 
-   void GeneratePlane(Vector2 size, int resolution)
-   {
-      vertices = new List<Vector3>();
-      float xPerStep = size.x / resolution;
-      float yPerStep = size.y / resolution;
+    void GeneratePlane(Vector2 size, int resolution)
+    {
+        vertices = new List<Vector3>();
+        float xPerStep = size.x / resolution;
+        float yPerStep = size.y / resolution;
 
-      for (int y = 0; y < resolution + 1; y++)
-      {
-         for (int x = 0; x < resolution + 1; x++)
-         {
-            vertices.Add(new Vector3(x*xPerStep, 0, y*yPerStep));
-         }  
-      }
+        for (int y = 0; y < resolution + 1; y++)
+        {
+            for (int x = 0; x < resolution + 1; x++)
+            {
+                vertices.Add(new Vector3(x * xPerStep, 0, y * yPerStep));
+            }
+        }
 
-      triangles = new List<int>();
-      for (int row = 0; row < resolution; row++)
-      {
-         for (int column = 0; column < resolution; column++)
-         {
-            int i = (row * resolution) + row + column;
-            
-            triangles.Add(i);
-            triangles.Add(i + resolution + 1);
-            triangles.Add(i + resolution + 2);
-            
-            triangles.Add(i);
-            triangles.Add(i+resolution+2);
-            triangles.Add(i + 1);
-         }
-      }
-   }
+        triangles = new List<int>();
+        for (int row = 0; row < resolution; row++)
+        {
+            for (int column = 0; column < resolution; column++)
+            {
+                int i = (row * resolution) + row + column;
 
-   void SaveMesh()
-   {
-      currentMesh = new Mesh();
-      currentMesh.Clear();
-      currentMesh.vertices = vertices.ToArray();
-      currentMesh.triangles = triangles.ToArray();
+                triangles.Add(i);
+                triangles.Add(i + resolution + 1);
+                triangles.Add(i + resolution + 2);
 
-      if (meshName != "")
-      {
-         Mesh findMesh = (Mesh)AssetDatabase.LoadAssetAtPath($"Assets/_Dev/DJN/DJN_Assets/{meshName}.mesh", typeof(Mesh));
-         
-         if (findMesh == null)
-         {
-            AssetDatabase.CreateAsset(currentMesh, $"Assets/_Dev/DJN/DJN_Assets/{meshName}.mesh");
+                triangles.Add(i);
+                triangles.Add(i + resolution + 2);
+                triangles.Add(i + 1);
+            }
+        }
+    }
 
-            meshName = "";
-         }
-         else
-         {
-            Debug.LogError("This asset name already exist.");
-         }
-      }
-   }
+    void SaveMesh()
+    {
+        currentMesh = new Mesh();
+        currentMesh.Clear();
+        currentMesh.vertices = vertices.ToArray();
+        currentMesh.triangles = triangles.ToArray();
+#if UNITY_EDITOR
+        if (meshName != "")
+        {
+            Mesh findMesh =
+                (Mesh)AssetDatabase.LoadAssetAtPath($"Assets/_Dev/DJN/DJN_Assets/{meshName}.mesh", typeof(Mesh));
+
+            if (findMesh == null)
+            {
+                AssetDatabase.CreateAsset(currentMesh, $"Assets/_Dev/DJN/DJN_Assets/{meshName}.mesh");
+
+                meshName = "";
+            }
+            else
+            {
+                Debug.LogError("This asset name already exist.");
+            }
+        }
+#endif
+    }
 }
