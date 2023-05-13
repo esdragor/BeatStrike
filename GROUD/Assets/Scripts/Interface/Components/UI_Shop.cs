3cb3c95bbf0426bcfdf8d36619d12f0ee81c0e12
baudouinlapeze <baudouinlapeze9@gmail.com>
2023-05-13 20:05:03 +0200

    Add Chest and Key system

--- GROUD/Assets/Scripts/Interface/Components/UI_Shop.cs
+++ GROUD/Assets/Scripts/Interface/Components/UI_Shop.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -23,6 +24,11 @@ public class UI_Shop : MonoBehaviour
     [SerializeField] private TMP_Text timeToReload;
     [Header("Gear Gold")] [SerializeField] private Transform ParentGoldGear;
     [SerializeField] private GameObject GearPrefabShop;
+    [Header("Gear Key")] [SerializeField] private Button commonChest;
+    [SerializeField] private Button epicChest;
+    [SerializeField] private Transform parentLootGearChest;
+    [SerializeField] private int ticketPriceCommon = 1;
+    [SerializeField] private int ticketPriceEpic = 5;
 
     private float decal = 5000f;
     private float timeToReloadValue = -99f;
@@ -41,6 +47,51 @@ public class UI_Shop : MonoBehaviour
 
         openBtn.onClick.AddListener(() => GameManager.onUpdated += CheckTime);
         exitBtn.onClick.AddListener(() => GameManager.onUpdated -= CheckTime);
+
+        commonChest.onClick.AddListener(OpenCommonChest);
+        epicChest.onClick.AddListener(OpenEpicChest);
+    }
+
+    public void PrintChest(int ID)
+    {
+        Inventory.AddItemOnInventory(ID);
+
+        if (parentLootGearChest.childCount > 0)
+            Destroy(parentLootGearChest.GetChild(0).gameObject);
+
+        GearDescription gd = Instantiate(GearPrefabShop, parentLootGearChest).GetComponent<GearDescription>();
+        gd.clickable = false;
+        Gear gear = Inventory.GetGearsData().First(g => g.ID == ID);
+        gd.GetComponent<Image>().sprite = gear.gearSprite;
+        gd.gear = ScriptableObject.CreateInstance<Gear>();
+        gd.gear.CopyGear(gear);
+        Destroy(gd.gameObject, 5f);
+    }
+
+    private void OpenCommonChest()
+    {
+        if (CurrencyManager.GetKeys() >= ticketPriceCommon)
+        {
+            CurrencyManager.RemoveKeys(ticketPriceCommon);
+
+            Gear[] gears = Inventory.GetGearsData().Where(gear => gear.rarity == Rarity.Common).ToArray();
+
+            int ID = gears[Random.Range(0, gears.Length)].ID;
+            PrintChest(ID);
+        }
+    }
+
+    private void OpenEpicChest()
+    {
+        if (CurrencyManager.GetKeys() >= ticketPriceEpic)
+        {
+            CurrencyManager.RemoveKeys(ticketPriceEpic);
+            Gear[] gears = Inventory.GetGearsData().Where(gear => gear.rarity <= Rarity.Epic).ToArray();
+
+            int ID = gears[Random.Range(0, gears.Length)].ID;
+
+            PrintChest(ID);
+        }
     }
 
     private void UpdateKeyText(int key)
@@ -55,11 +106,11 @@ public class UI_Shop : MonoBehaviour
 
     private void PrintGoldGear()
     {
+        Gear[] gears = Inventory.GetGearsData();
         for (int i = 0; i < 3; i++)
         {
             GearDescription gd = Instantiate(GearPrefabShop, ParentGoldGear).GetComponent<GearDescription>();
             gd.OnSell = true;
-            Gear[] gears = Inventory.GetGearsData();
             Gear gear = gears[Random.Range(0, gears.Length)];
             gd.GetComponent<Image>().sprite = gear.gearSprite;
             gd.gear = ScriptableObject.CreateInstance<Gear>();
