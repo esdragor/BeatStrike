b91297dd06ea1e632f295d775e2551b7c43c5aa4
esdragor <64788862+esdragor@users.noreply.github.com>
2023-06-01 14:13:21 +0200

    change structure to BPM

--- GROUD/Assets/Scripts/Manager/SoundManager.cs
+++ GROUD/Assets/Scripts/Manager/SoundManager.cs
@@ -17,16 +17,20 @@ public class SoundManager : MonoBehaviour
     private static SoundManager instance;
 
     [SerializeField] private SoundData struct60BPM;
+    [SerializeField] private SoundData struct80BPM;
     [SerializeField] private SoundData struct90BPM;
+    [SerializeField] private SoundData struct100BPM;
     [SerializeField] private SoundData struct120BPM;
+    [SerializeField] private SoundData struct140BPM;
+    [SerializeField] private SoundData struct160BPM;
     [SerializeField] private SoundData struct180BPM;
-    [SerializeField] private SoundData struct240BPM;
-    
-    [Header("Musical Background")]
-    [SerializeField] private AudioSource audioSourceMusicalBackground;
-    
-    [Header("Note Player")]
-    [SerializeField] private AudioSource[] audioSourceNotePlayer;
+    [SerializeField] private SoundData struct200BPM;
+
+    [Header("Musical Background")] [SerializeField]
+    private AudioSource audioSourceMusicalBackground;
+
+    [Header("Note Player")] [SerializeField]
+    private AudioSource[] audioSourceNotePlayer;
 
     private void Awake()
     {
@@ -40,15 +44,20 @@ public class SoundManager : MonoBehaviour
         instance.audioSourceMusicalBackground.clip = BPM switch
         {
             60 => instance.struct60BPM.clipBackground,
+            80 => instance.struct80BPM.clipBackground,
             90 => instance.struct90BPM.clipBackground,
+            100 => instance.struct100BPM.clipBackground,
             120 => instance.struct120BPM.clipBackground,
+            140 => instance.struct140BPM.clipBackground,
+            160 => instance.struct160BPM.clipBackground,
             180 => instance.struct180BPM.clipBackground,
-            240 => instance.struct240BPM.clipBackground,
-            _ => instance.audioSourceMusicalBackground.clip
+            200 => instance.struct200BPM.clipBackground,
+            _ => null
         };
-        instance.audioSourceMusicalBackground.Play();
+        if (instance.audioSourceMusicalBackground.clip)
+            instance.audioSourceMusicalBackground.Play();
     }
-    
+
     public static void PlayRandomGoodNotePlayer(int BPM)
     {
         int sizeAudioSource = instance.audioSourceNotePlayer.Length;
@@ -59,19 +68,28 @@ public class SoundManager : MonoBehaviour
             source = instance.audioSourceNotePlayer[i];
             break;
         }
+
         if (source == null) return;
         source.clip = BPM switch
         {
             60 => instance.struct60BPM.clipNotePlayerGood[
                 Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
+            80 => instance.struct80BPM.clipNotePlayerGood[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
             90 => instance.struct90BPM.clipNotePlayerGood[
-                Random.Range(0, instance.struct90BPM.clipNotePlayerGood.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
+            100 => instance.struct100BPM.clipNotePlayerGood[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
             120 => instance.struct120BPM.clipNotePlayerGood[
-                Random.Range(0, instance.struct120BPM.clipNotePlayerGood.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
+            140 => instance.struct140BPM.clipNotePlayerGood[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
+            160 => instance.struct60BPM.clipNotePlayerGood[
+                Random.Range(0, instance.struct160BPM.clipNotePlayerGood.Length)],
             180 => instance.struct180BPM.clipNotePlayerGood[
-                Random.Range(0, instance.struct180BPM.clipNotePlayerGood.Length)],
-            240 => instance.struct240BPM.clipNotePlayerGood[
-                Random.Range(0, instance.struct240BPM.clipNotePlayerGood.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
+            200 => instance.struct200BPM.clipNotePlayerGood[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerGood.Length)],
             _ => null
         };
         if (source.clip)
@@ -88,19 +106,28 @@ public class SoundManager : MonoBehaviour
             source = instance.audioSourceNotePlayer[i];
             break;
         }
+
         if (source == null) return;
         source.clip = BPM switch
         {
             60 => instance.struct60BPM.clipNotePlayerBad[
                 Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
+            80 => instance.struct80BPM.clipNotePlayerBad[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
             90 => instance.struct90BPM.clipNotePlayerBad[
-                Random.Range(0, instance.struct90BPM.clipNotePlayerBad.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
+            100 => instance.struct100BPM.clipNotePlayerBad[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
             120 => instance.struct120BPM.clipNotePlayerBad[
-                Random.Range(0, instance.struct120BPM.clipNotePlayerBad.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
+            140 => instance.struct140BPM.clipNotePlayerBad[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
+            160 => instance.struct60BPM.clipNotePlayerBad[
+                Random.Range(0, instance.struct160BPM.clipNotePlayerBad.Length)],
             180 => instance.struct180BPM.clipNotePlayerBad[
-                Random.Range(0, instance.struct180BPM.clipNotePlayerBad.Length)],
-            240 => instance.struct240BPM.clipNotePlayerBad[
-                Random.Range(0, instance.struct240BPM.clipNotePlayerBad.Length)],
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
+            200 => instance.struct200BPM.clipNotePlayerBad[
+                Random.Range(0, instance.struct60BPM.clipNotePlayerBad.Length)],
             _ => null
         };
         if (source.clip)
@@ -108,13 +135,14 @@ public class SoundManager : MonoBehaviour
     }
 
     private static bool isMute;
+
     public static bool ToggleMute()
     {
         isMute = !isMute;
-        
+
         instance.audioSourceMusicalBackground.mute = isMute;
         foreach (var s in instance.audioSourceNotePlayer)
-        {   
+        {
             s.mute = isMute;
         }
 
